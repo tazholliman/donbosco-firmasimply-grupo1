ad20abb6e07cf05a8da5b3fc43a9d9af5ec81c0a
unknown <joaquincabezasberjillos@gmail.com>
2021-06-30 15:08:38 +0200

    Actualizacion

--- assets/dist/js/agregarPildora.js
+++ assets/dist/js/agregarPildora.js
@@ -1,79 +1,203 @@
-class Book {
-    constructor(nombre, descripcion, presentacion){
-        this.nombre = nombre;
-        this.descripcion = descripcion;
-        this.presentacion = presentacion;
+const todoForm = document.querySelector(".todo-form");
+const todoList = document.querySelector(".todo-list");
+const totalTasks = document.querySelector(".total-tasks span");
+const completedTasks = document.querySelector(".completed-tasks span");
+const remainingTasks = document.querySelector(".remaining-tasks span");
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+if (localStorage.getItem("tasks")) {
+  tasks.map((task) => {
+    createTask(task);
+  });
+}
+
+// submit form
+todoForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const input = this.name;
+  const inputValue = input.value;
+
+  if (inputValue != "") {
+    const task = {
+      id: new Date().getTime(),
+      name: inputValue,
+      isCompleted: false
+    };
+
+    tasks.push(task);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    createTask(task);
+    todoForm.reset();
+  }
+  input.focus();
+});
+
+// remove task
+todoList.addEventListener("click", (e) => {
+  if (
+    e.target.classList.contains("remove-task") ||
+    e.target.parentElement.classList.contains("remove-task")
+  ) {
+    const taskId = e.target.closest("li").id;
+    removeTask(taskId);
+  }
+});
+
+// update task - change status or name
+todoList.addEventListener("input", (e) => {
+  const taskId = e.target.closest("li").id;
+  updateTask(taskId, e.target);
+});
+
+// prevent new lines with Enter
+todoList.addEventListener("keydown", function (e) {
+  if (e.keyCode === 13) {
+    e.preventDefault();
+  }
+});
+
+// create task
+function createTask(task) {
+  const taskEl = document.createElement("li");
+  taskEl.setAttribute("id", task.id);
+  const taskElMarkup = `
+    <div class="checkbox-wrapper">
+      <input type="checkbox" id="${task.name}-${task.id}" name="tasks" ${
+    task.isCompleted ? "checked" : ""
+  }>
+      <label for="${task.name}-${task.id}">
+        <svg class="checkbox-empty">
+          <use xlink:href="#checkbox_empty"></use>
+        </svg>
+        <svg class="checkmark">
+          <use xlink:href="#checkmark"></use>
+        </svg>
+      </label>
+      <span ${!task.isCompleted ? "contenteditable" : ""}>${task.name}</span>
+    </div>
+    <button class="remove-task" title="Remove ${task.name} task">
+      <svg>
+        <use xlink:href="#close"></use>
+      </svg>
+    </button>
+  `;
+  taskEl.innerHTML = taskElMarkup;
+  todoList.appendChild(taskEl);
+  countTasks();
+}
+
+// remove task
+function removeTask(taskId) {
+  tasks = tasks.filter((task) => task.id !== parseInt(taskId));
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  document.getElementById(taskId).remove();
+  countTasks();
+}
+
+// update task
+function updateTask(taskId, el) {
+  const task = tasks.find((task) => task.id === parseInt(taskId));
+
+  if (el.hasAttribute("contentEditable")) {
+    task.name = el.textContent;
+  } else {
+    const span = el.nextElementSibling.nextElementSibling;
+    task.isCompleted = !task.isCompleted;
+    if (task.isCompleted) {
+      span.removeAttribute("contenteditable");
+      el.setAttribute("checked", "");
+    } else {
+      el.removeAttribute("checked");
+      span.setAttribute("contenteditable", "");
     }
   }
-
-const defaultBooks = [
-	{
-		nombre: 'Book 1',
-		descripcion: 'Brad Traversy',
-		presentacion: '12345'
-	},
-	{
-		nombre: 'Book 2',
-		descripcion: 'Mehul Mohan',
-		presentacion: '6789'
-	}
-]
-
-class UI {
-    static displayBooks(){
-        defaultBooks.forEach(book => UI.addBookToList(book))
-    }
-    static clearFileds(){
-        document.getElementById("nombre").value = ""
-        document.getElementById("descripcion").value = ""
-        document.getElementById("presentacion").value = ""
-    }
-    static deleteBook(target){
-        if (target.classList.contains('delete')) {
-			// we clicked the X icon
-			target.parentElement.parentElement.remove()
-		}
-    }
-    static addBookToList(book) {
-        const list = document.getElementById("book-list") // get the #book-list DOM node here
-        const row = document.createElement("tr")// create a TR row element here (document.createElement)
-        row.innerHTML = `
-        <td>${book.nombre}</td>
-        <td>${book.descripcion}</td>
-        <td>${book.presentacion}</td>
-        <td></td>
-        <td>${Date()}</td>
-        <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
-        `
-        list.appendChild(row)
-    }
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  countTasks();
 }
 
-UI.displayBooks()
+function countTasks() {
+  totalTasks.textContent = tasks.length;
+  const completedTasksArray = tasks.filter((task) => task.isCompleted === true);
+  completedTasks.textContent = completedTasksArray.length;
+  remainingTasks.textContent = tasks.length - completedTasksArray.length;
+}
 
-document.querySelector('#book-form').addEventListener('submit', addABook, false)
+// class Pildora {
+//     constructor(nombre, descripcion, presentacion){
+//         this.nombre = nombre;
+//         this.descripcion = descripcion;
+//         this.presentacion = presentacion;
+//     }
+//   }
 
-function addABook(e) {
-	// prevent actual submission
-	e.preventDefault()
+// const defaultPildoras = [
+// 	{
+// 		nombre: 'Pildora 1',
+// 		descripcion: 'Brad Traversy',
+// 		presentacion: '12345'
+// 	},
+// 	{
+// 		nombre: 'Pildora 2',
+// 		descripcion: 'Mehul Mohan',
+// 		presentacion: '6789'
+// 	}
+// ]
 
-	// Capturar los valores del Form
-    const nombre = document.getElementById("nombre").value
-    const descripcion = document.getElementById("descripcion").value
-    const presentacion = document.getElementById("presentacion").value
-	// Crear un nuevo objeto book
-    const book = new Book(nombre, descripcion, presentacion)
+// class UI {
+//     static displayPildoras(){
+//         defaultPildoras.forEach(pildora => UI.addPildoraToList(pildora))
+//     }
+//     static clearFileds(){
+//         document.getElementById("nombre").value = ""
+//         document.getElementById("descripcion").value = ""
+//         document.getElementById("presentacion").value = ""
+//     }
+//     static deletePildora(target){
+//         if (target.classList.contains('delete')) {
+// 			// we clicked the X icon
+// 			target.parentElement.parentElement.remove()
+// 		}
+//     }
+//     static addPildoraToList(pildora) {
+//         const list = document.getElementById("pildora-list") // get the #pildora-list DOM node here
+//         const row = document.createElement("tr")// create a TR row element here (document.createElement)
+//         row.innerHTML = `
+//         <td>${pildora.nombre}</td>
+//         <td>${pildora.descripcion}</td>
+//         <td>${pildora.presentacion}</td>
+//         <td><input type="checkbox"></td>
+//         <td>${Date()}</td>
+//         <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+//         `
+//         list.appendChild(row)
+//     }
+// }
 
-    //Aunque no tenga un valor definido en la funcion, se encarga de darle este valor por defecto a cuaqluier variable que se asigne después.
+// UI.displayPildoras()
 
-	// Añadir el objeto book creado a UI (mostrarlo en HTML)
-    UI.addBookToList(book)
+// document.querySelector('#pildora-form').addEventListener('submit', addAPildora, false)
 
-    UI.clearFileds()
+// function addAPildora(e) {
+// 	// prevent actual submission
+// 	e.preventDefault()
+
+// 	// Capturar los valores del Form
+//     const nombre = document.getElementById("nombre").value
+//     const descripcion = document.getElementById("descripcion").value
+//     const presentacion = document.getElementById("presentacion").value
+// 	// Crear un nuevo objeto pildora
+//     const pildora = new Pildora(nombre, descripcion, presentacion)
+
+//     //Aunque no tenga un valor definido en la funcion, se encarga de darle este valor por defecto a cuaqluier variable que se asigne después.
+
+// 	// Añadir el objeto pildora creado a UI (mostrarlo en HTML)
+//     UI.addPildoraToList(pildora)
+
+//     UI.clearFileds()
     
-}
-    document.getElementById('book-list').addEventListener('click', handleRemove)
-    function handleRemove(e) {
-        // Remove book from UI
-        UI.deleteBook(e.target)
-    }
+// }
+//     document.getElementById('pildora-list').addEventListener('click', handleRemove)
+//     function handleRemove(e) {
+//         // Remove pildora from UI
+//         UI.deletePildora(e.target)
+//     }
