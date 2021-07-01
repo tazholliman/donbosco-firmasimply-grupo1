4e86d1bc25f321f246f03c777bf3d9bbb52b5a1c
rosario gamer <82204530+rosariogamer@users.noreply.github.com>
2021-07-01 12:57:42 +0200

    cambios

--- assets/dist/js/app-b.js
+++ assets/dist/js/app-b.js
@@ -1,142 +1,142 @@
-class Book {
-  constructor(nombre, categoria, descripcion, fecha) {
-    this.nombre = nombre;
-    this.categoria = categoria;
-    this.descripcion = descripcion;
-    this.fecha = fecha;
-  }
-}
+//  class Book {
+//    constructor(nombre, categoria, descripcion, fecha) {
+//     this.nombre = nombre;
+//     this.categoria = categoria;
+//     this.descripcion = descripcion;
+//     this.fecha = fecha;
+//   }
+// }
 
-// UI Class: Handle UI Tasks
-class UI {
-  static displayBooks() {
-    const books = Store.getBooks();
 
-    books.forEach((book) => UI.addBookToList(book));
-  }
+// class UI {
+//   static displayBooks() {
+//     const books = Store.getBooks();
 
-  static addBookToList(book) {
-    const list = document.querySelector('#book-list');
+//     books.forEach((book) => UI.addBookToList(book));
+//   }
 
-    const row = document.createElement('tr');
+//   static addBookToList(book) {
+//     const list = document.querySelector('#book-list');
 
-    row.innerHTML = `
-      <td>${book.nombre}</td>
-      <td>${book.categoria}</td>
-      <td>${book.descripcion}</td>
-      <td>${book.fecha}</td>
+//     const row = document.createElement('tr');
+
+//     row.innerHTML = `
+//       <td>${book.nombre}</td>
+//       <td>${book.categoria}</td>
+//       <td>${book.descripcion}</td>
+//       <td>${book.fecha}</td>
  
-      <td></td>
-      <td></td>
-      <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
-  <td></td>
-    `;
+//       <td></td>
+//       <td><input type="checkbox"></td>
+//       <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+//   <td></td>
+//     `;
 
-    list.appendChild(row);
-  }
+//     list.appendChild(row);
+//   }
 
-   static deleteBook(el) {
-     if (el.classList.contains('delete')) {
-      el.parentElement.parentElement.remove();
-     }
-   }
+//    static deleteBook(el) {
+//      if (el.classList.contains('delete')) {
+//       el.parentElement.parentElement.remove();
+//      }
+//    }
 
-  static showAlert(message, className) {
-    const div = document.createElement('div');
-    div.className = `alert alert-${className}`;
-    div.appendChild(document.createTextNode(message));
-    const container = document.querySelector('.container');
-    const form = document.querySelector('#book-form');
+//   static showAlert(message, className) {
+//     const div = document.createElement('div');
+//     div.className = `alert alert-${className}`;
+//     div.appendChild(document.createTextNode(message));
+//     const container = document.querySelector('.container');
+//     const form = document.querySelector('#book-form');
     
 
-    // Vanish in 3 seconds
+//     // Vanish in 3 seconds
     
-  }
+//   }
 
-  static clearFields() {
-    document.querySelector('#nombre').value = '';
-    document.querySelector('#categoria').value = '';
-    document.querySelector('#descripcion').value = '';
-    document.querySelector('#fecha').value = '';
-  }
-}
+//   static clearFields() {
+//     document.querySelector('#nombre').value = '';
+//     document.querySelector('#categoria').value = '';
+//     document.querySelector('#descripcion').value = '';
+//     document.querySelector('#fecha').value = '';
+//   }
+// }
 
-// Store Class: Handles Storage
-class Store {
-  static getBooks() {
-    let books;
-    if (localStorage.getItem('books') === null) {
-      books = [];
-    } else {
-      books = JSON.parse(localStorage.getItem('books'));
-    }
+// // Store Class: Handles Storage
+// class Store {
+//   static getBooks() {
+//     let books;
+//     if (localStorage.getItem('books') === null) {
+//       books = [];
+//     } else {
+//       books = JSON.parse(localStorage.getItem('books'));
+//     }
 
-    return books;
-  }
+//     return books;
+//   }
 
-  static addBook(book) {
-    const books = Store.getBooks();
-    books.push(book);
-    localStorage.setItem('books', JSON.stringify(books));
-  }
+//   static addBook(book) {
+//     const books = Store.getBooks();
+//     books.push(book);
+//     localStorage.setItem('books', JSON.stringify(books));
+//   }
 
-  static removeBook(descripcion) {
-    const books = Store.getBooks();
+//   static removeBook(descripcion) {
+//     const books = Store.getBooks();
 
-    books.forEach((book, index) => {
-      if (book.descripcion === descripcion) {
-        books.splice(index, 1);
-      }
-    });
+//     books.forEach((book, index) => {
+//       if (book.descripcion === descripcion) {
+//         books.splice(index, 1);
+//       }
+//     });
 
-    localStorage.setItem('books', JSON.stringify(books));
-  }
-}
+//     localStorage.setItem('books', JSON.stringify(books));
+//   }
+// }
 
 
-// Event: Display Books
-document.addEventListener('DOMContentLoaded', UI.displayBooks);
+// // Event: Display Books
+// document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
-// Event: Add a Book
-document.querySelector('#book-form').addEventListener('submit', (e) => {
-  // Prevent actual submit
-  e.preventDefault();
+// // Event: Add a Book
+// document.querySelector('#book-form').addEventListener('submit', (e) => {
+//   // Prevent actual submit
+//   e.preventDefault();
 
-  // Get form values
-  const nombre = document.querySelector('#nombre').value;
-  const categoria = document.querySelector('#categoria').value;
-  const descripcion = document.querySelector('#descripcion').value;
-  const fecha = document.querySelector('#fecha').value;
+//   // Get form values
+//   const nombre = document.querySelector('#nombre').value;
+//   const categoria = document.querySelector('#categoria').value;
+//   const descripcion = document.querySelector('#descripcion').value;
+//   const fecha = document.querySelector('#fecha').value;
 
-  // Validate
-  if (nombre === '' || categoria === '' || descripcion === ''  || fecha === '') {
-    UI.showAlert('Please fill in all fields', 'danger');
-  } else {
-    // Instatiate book
-    const book = new Book(nombre, categoria, descripcion, fecha);
+//   // Validate
+//   if (nombre === '' || categoria === '' || descripcion === ''  || fecha === '') {
+//     UI.showAlert('Please fill in all fields', 'danger');
+//   } else {
+//     // Instatiate book
+//     const book = new Book(nombre, categoria, descripcion, fecha);
 
-    // Add Book to UI
-    UI.addBookToList(book);
+//     // Add Book to UI
+//     UI.addBookToList(book);
 
-    // Add book to store
-    Store.addBook(book);
+//     // Add book to store
+//     Store.addBook(book);
 
-    // Show success message
-    UI.showAlert('Book Added', 'success');
+//     // Show success message
+//     UI.showAlert('Book Added', 'success');
 
-    // Clear fields
-    UI.clearFields();
-  }
-});
+//     // Clear fields
+//     UI.clearFields();
+//   }
+// });
 
-// Event: Remove a Book
-document.querySelector('#book-list').addEventListener('click', (e) => {
-  // Remove book from UI
-  UI.deleteBook(e.target);
+// // Event: Remove a Book
+// document.querySelector('#book-list').addEventListener('click', (e) => {
+//   // Remove book from UI
+//   UI.deleteBook(e.target);
 
-  // Remove book from store
-  Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
+//   // Remove book from store
+//   Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
 
-  // Show success message
-  UI.showAlert('Book Removed', 'success');
-});
+//   // Show success message
+//   UI.showAlert('Book Removed', 'success');
+//  });
--- assets/js/firmasimply/tareas.js
+++ assets/js/firmasimply/tareas.js
@@ -1,8 +1,147 @@
 import Auth from './Modules/Auth/Auth.js';
-import Tarea from './Modules/Tarea.js';
+import tarea from './Modules/Tarea.js';
 import Categoria from './Modules/Categoria.js';
+import { listadoTareas } from './Modules/API/llamadasApi.js';
+// Funcionalidad mostrar listado de tareas
+class Tarea {
+    constructor(nombre, categoria, descripcion, fecha) {
+      this.nombre = nombre;
+      this.categoria = categoria;
+      this.descripcion = descripcion;
+      this.fecha = fecha;
+    }
+  }
+  
+  // UI Class: Handle UI Tasks
+  class UI {
+    static displayTarea() {
+      const getTarea = Store.getTarea();
+  
+      tarea.forEach((tarea) => UI.addTareasToList(tarea));
+    }
+  
+    static addTareasToList(tarea) {
+      const list = document.querySelector('#tarea-list');
+  
+      const row = document.createElement('tr');
+  
+      row.innerHTML = `
+        <td>${tarea.nombre}</td>
+        <td>${tarea.categoria}</td>
+        <td>${tarea.descripcion}</td>
+        <td>${tarea.fecha}</td>
+   
+        <td><input type="checkbox"></td>
+        <td></td>
+        <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+    <td></td>
+      `;
+  
+      list.appendChild(row);
+    }
+  
+     static deleteTarea(el) {
+       if (el.classList.contains('delete')) {
+        el.parentElement.parentElement.remove();
+       }
+     }
+  
+    static showAlert(message, className) {
+      const div = document.createElement('div');
+      div.className = `alert alert-${className}`;
+      div.appendChild(document.createTextNode(message));
+      const container = document.querySelector('.container');
+      const form = document.querySelector('#tarea-form');
+      
+  
+      // Vanish in 3 seconds
+      
+    }
+  
+    static clearFields() {
+      document.querySelector('#nombre').value = '';
+      document.querySelector('#categoria').value = '';
+      document.querySelector('#descripcion').value = '';
+      document.querySelector('#fecha').value = '';
+    }
+  }
+  
+  // Store Class: Handles Storage
+  class Store {
+    static getTarea() {
+      let tarea;
+      // if (localStorage.getItem('books') === null) {
+        // books = [];
+      // } else {
+        // books = JSON.parse(localStorage.getItem('books'));
+      }
+  
+      // return books;
+    // }
+  
+    static addTarea(tarea) {
+      const tarea = Store.getTarea();
+      tarea.push(tarea);
+      localStorage.setItem('tarea', JSON.stringify(tarea));
+    }
+  
+    static removetarea(descripcion) {
+      const tarea = Store.getTarea();
+  
+      tarea.forEach((tarea, index) => {
+        if (tarea.descripcion === descripcion) {
+          tarea.splice(index, 1);
+        }
+      });
+  
+      localStorage.setItem('tarea', JSON.stringify(tarea));
+    }
+  }
+  
+  
+ 
+  document.addEventListener('DOMContentLoaded', UI.displayTarea);
 
+  document.querySelector('#tarea-form').addEventListener('submit', (e) => {
+    // Prevent actual submit
+    e.preventDefault();
+  
+    // Get form values
+    const nombre = document.querySelector('#nombre').value;
+    const categoria = document.querySelector('#categoria').value;
+    const descripcion = document.querySelector('#descripcion').value;
+    const fecha = document.querySelector('#fecha').value;
+  
+    // Validate
+    if (nombre === '' || categoria === '' || descripcion === ''  || fecha === '') {
+      UI.showAlert('Please fill in all fields', 'danger');
+    } else {
+    
+      const tarea = new Tarea(nombre, categoria, descripcion, fecha);
+  
+      UI.addTareaToList(tarea);
+  
+      Store.addtarea(tarea);
+  
+      UI.showAlert('tarea Added', 'success');
+  
+      UI.clearFields();
+    }
+  });
+  
+ 
+  document.querySelector('#tarea-list').addEventListener('click', (e) => {
+   
+    UI.deleteTarea(e.target);
+  
 
+    Store.removeTarea(e.target.parentElement.previousElementSibling.textContent);
+  
+    UI.showAlert('Tarea Removed', 'success');
+  });
+  
+
+// Funcionalidad mostrar listado de tareas
 // Funcionalidad mostrar listado de tareas
 
 // Funcionalidad crear una tarea
