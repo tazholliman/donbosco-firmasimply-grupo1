89f3634bdc41e21d8e4c7c7537a9be7829e31b3b
Alicia Estrada <ali07marzo@gmail.com>
2021-07-07 14:06:45 +0200

    nuevo index y js

--- assets/js/firmasimply/index.js
+++ assets/js/firmasimply/index.js
@@ -3,28 +3,63 @@ import Asistencia from './Modules/Asistencia.js';
 import Tarea from './Modules/Tarea.js';
 import Pildora from './Modules/Pildora.js';
 
-getListadoFirmas();
-async function getListadoFirmas() {
-    let respuesta = await Asistencia.getlistadoFirmas();
-    console.log(respuesta);
-    // Tu Código
-}
+
+// Tu Código
+
 
 // Funcionalidad de Firmar
+let firmaEntrada = document.getElementById("entrada")
 
-function firmar() {
-    document.getElementById('btn').onclick;
-    firmar();
+firmaEntrada.addEventListener("click", async (e) => {
+    e.preventDefault()
+
+    let firma = {
+        user_id: Auth.getCoder().id,
+        nota: 'texto test',
+        estado: 1 // 1 para entrada, 0 para salida
+  
+};
+await Asistencia.firmar(firma);
+
+
+// Consultar listado Firmas
+
+async function listadoFirmas() {
+    let listadoFirmas = await Asistencia.getlistadoFirmas();
+
+    // Pasarlo de javascript al html     
+    let lista = document.getElementById('lista_firmas')
+    let comentario = document.getElementById("anotacion").value
+    lista.innerHTML += `<li> ${listadoFirmas[0].created_at + " " + "anotacion: " + nota} </li>`;
+
+
+}
+//Firmar  salida
+let firmaSalida = document.getElementById("salida")
+
+firmaSalidas.addEventListener("click", async (e) => {
+    e.preventDefault()
+    let firma = {
+        user_id: Auth.getCoder().id,
+        nota: 'texto test',
+        estado: 0
+    }
+};
+
+await Asistencia.firmar(firma);
+
+// Consultar listado Píldoras
+
+
+async function listadoPildoras() {
+    let listadoPildoras = await Asistencia.getlistadoPildoras();
+
 }
 
-// Funcionalidad mostrar Listado Tareas
-async function getListadoTareass() {
-    let respuesta = await Asistencia.getlistadoTareas();
-    console.log(respuesta);
-}
-// Funcionalidad mostrar Listado Píldoras
-async function getListadoPildoras() {
-    let respuesta = await Asistencia.getlistadoPildoras();
-    console.log(respuesta);
-    // Tu Código
+
+
+// Consultar listado Tareas
+
+async function listadoTareas() {
+    let listadoTareas = await Asistencia.getlistadoTareas();
 }
